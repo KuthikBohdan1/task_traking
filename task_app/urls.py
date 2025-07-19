b2ad6489902687c36dcf6932f726b742f479120c
KuthikBohdan1 <bkuzik14@gmail.com>
2025-07-20 00:15:38 +0300

    1

--- task_app/urls.py
+++ task_app/urls.py
@@ -4,6 +4,6 @@ from task_app import views
 urlpatterns = [
    
     path("", views.TaskListView.as_view(), name = "task_list" ),
-    path("<int: pk>/", views.TaskDetailView.as_view(), name = "task_datail" ),
+    path("<int:pk>/", views.TaskDetailView.as_view(), name = "task_detail" ),
 
 ] 
--- task_app/views.py
+++ task_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from .models import Task, Comment
 from task_app import models
 from django.views.generic import ListView, DetailView, CreateView
@@ -12,3 +12,10 @@ class TaskDetailView(DetailView):
     model = models.Task
     context_object_name = "task"
     template_name = "tasks/task_detail.html"
+
+
+class TaskCreateView(CreateView):
+    model = models.Task
+    template_name = "tasks/task_form.hmtl"
+    form_class = TaskForm
+    success_url = reverse("task_list")

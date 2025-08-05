59adfd224fccb315b82738ad38c2dc37acb83038
KuthikBohdan1 <bkuzik14@gmail.com>
2025-08-05 16:57:52 +0300

    rewthgj,m

--- project/settings.py
+++ project/settings.py
@@ -132,9 +132,11 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 LOGIN_URL = "/login/"
-LOGIN_REDIRECT_URL = "task-list"
-LOGOUT_REDIRECT_URL ="/login/"
+LOGIN_REDIRECT_URL = "tasks:task-list"
+LOGOUT_REDIRECT_URL ="tasks:logout"
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
--- task_app/views.py
+++ task_app/views.py
@@ -9,6 +9,11 @@ from task_app.mixins import UserIsOwnerMixin
 from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 class TaskListView(LoginRequiredMixin, ListView):
     model = models.Task
     context_object_name = "tasks"
@@ -38,13 +43,11 @@ class TaskListView(LoginRequiredMixin, ListView):
         context["form"] = TaskFilterForm(self.request.GET)
         return context
 
-
-
-
 class TaskDetailView(LoginRequiredMixin, DetailView):
     model = models.Task
     context_object_name = "task"
     template_name = "tasks/task_detail.html"
+
 
 
 
@@ -142,7 +145,35 @@ class TaskDeleteView(LoginRequiredMixin, UserIsOwnerMixin, DeleteView):
 
 
 
-class Comme
+class CommentLIkeToggle(LoginRequiredMixin, View):
+
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(models.Comment, pk=self.kwargs.get('pk'))
+        like_qs = models.Like.objects.filter(comment=comment, user=request.user)
+        if like_qs.exists():
+            like_qs.delete()
+        else:
+            models.Like.objects.create(comment=comment, user=request.user)
+        return HttpResponseRedirect(comment.get_absolute_url())
 
 
 
+class CustomLoginView(LoginView):
+    template_name = "tasks/custom_register/login.html"
+    redirect_authenticated_user = True
+    
+
+
+class CustomLogoutView(LogoutView):
+    next_page = "tasks:login"
+
+
+class RegisterView(CreateView):
+    template_name = "tasks/custom_register/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("tasks:login")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
